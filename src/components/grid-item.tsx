7e3c9bc0f5ad3786d772f3ceec151aae75358ce8
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2024-01-02 16:40:10 +0300

    add grid item component

---
+++ src/components/grid-item.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import {Product} from '../lib/definitions';
+import ProductTile from './tile';
+
+export default function GridItem({item, size}: {item: Product; size: 'full' | 'half'}) {
+  return (
+    <div className={size === 'full' ? 'md:col-span-4 md:row-span-2' : 'md:col-span-2 md:row-span-1'}>
+      <Link
+        className="relative block w-full h-full aspect-square"
+        href={`/product/${item.id}`}
+      >
+        <ProductTile
+          label={{
+            title: item.title,
+            amount: item.amount,
+            position: size === 'full' ? 'center' : 'bottom',
+          }}
+          src={item.image_url}
+          alt={item.title}
+          fill
+        />
+      </Link>
+    </div>
+  );
+}
