f97a7a85b0fcc5d45e6a20bdba09ce032ef17351
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2024-01-02 16:10:08 +0300

    add fetch products functions

---
+++ src/lib/data.ts
@@ -0,0 +1,35 @@
+import {sql} from '@vercel/postgres';
+import {Product} from './definitions';
+
+export async function fetchHomepageFeaturedProducts() {
+  try {
+    const data = await sql<Product>`SELECT * FROM product WHERE tag = 'homepage-featured-product'`;
+
+    return data.rows;
+  } catch (error) {
+    console.log('Database error: ', error);
+    throw new Error('Failed to fetch featured products data');
+  }
+}
+
+export async function fetchCommonProducts() {
+  try {
+    const data = await sql<Product>`SELECT * FROM product WHERE tag = 'common'`;
+
+    return data.rows?.slice(0, 5);
+  } catch (error) {
+    console.log('Database error: ', error);
+    throw new Error('Failed to fetch common products data');
+  }
+}
+
+export async function fetchAllProducts() {
+  try {
+    const data = await sql<Product>`SELECT * FROM product`;
+
+    return data.rows;
+  } catch (error) {
+    console.log('Database error: ', error);
+    throw new Error('Failed to fetch products data');
+  }
+}
