8659dfc49c1d99e87c32c30b859c054e8a85d0d4
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2024-01-03 10:27:28 +0300

    add home grid three items

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
-export default function Home() {
+import {fetchHomepageFeaturedProducts} from '@/src/lib/data';
+import GridItem from '@/src/components/grid-item';
+
+export default async function Home() {
+  const featuredProducts = await fetchHomepageFeaturedProducts();
+
+  if (!featuredProducts) return null;
+
+  const [firstProduct, secondProduct, thirdProduct] = featuredProducts;
+
   return (
     <main>
-      <p>Home page</p>
+      <section className="grid md:grid-cols-6 md:grid-rows-2 gap-4 px-4 mb-4 max-w-screen-lg mx-auto">
+        <GridItem
+          item={firstProduct}
+          size="full"
+        />
+        <GridItem
+          item={secondProduct}
+          size="half"
+        />
+        <GridItem
+          item={thirdProduct}
+          size="half"
+        />
+      </section>
     </main>
   );
 }
