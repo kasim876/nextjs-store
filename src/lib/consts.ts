d69010fd3dc5ce9c27b2bd6e3731062e2c562560
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2024-01-05 21:35:19 +0300

    add filter links

---
+++ src/lib/consts.ts
@@ -0,0 +1,39 @@
+import {PathFilterItem, SortFilterItem} from './definitions';
+
+export const defaultSort = {
+  title: 'Relevance',
+  slug: null,
+  sortKey: 'RELEVANCE',
+  reverse: false,
+};
+
+export const sortFilterLinks: SortFilterItem[] = [
+  defaultSort,
+  {
+    title: 'Price: Low to high',
+    slug: 'price-asc',
+    sortKey: 'PRICE',
+    reverse: true,
+  },
+  {
+    title: 'Price: High to low',
+    slug: 'price-desc',
+    sortKey: 'PRICE',
+    reverse: false,
+  },
+];
+
+export const pathFilterLinks: PathFilterItem[] = [
+  {title: 'All', href: '/search'},
+  {title: 'Bags', href: '/search/bags'},
+  {title: 'Drinkware', href: '/search/drinkware'},
+  {title: 'Electronics', href: '/search/electronics'},
+  {title: 'Footware', href: '/search/footware'},
+  {title: 'Headware', href: '/search/headware'},
+  {title: 'Hoodies', href: '/search/hoodies'},
+  {title: 'Jackets', href: '/search/jackets'},
+  {title: 'Kids', href: '/search/kids'},
+  {title: 'Pets', href: '/search/pets'},
+  {title: 'Shirts', href: '/search/shirts'},
+  {title: 'Stickers', href: '/search/stickers'},
+];
