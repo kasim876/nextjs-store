425fb3a3de070cf6fa5994413b0cab9c8217acb4
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2024-01-25 21:58:03 +0300

    update search component

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import {MagnifyingGlassIcon} from '@heroicons/react/24/outline';
-import {usePathname, useRouter, useSearchParams} from 'next/navigation';
+import {useRouter, useSearchParams} from 'next/navigation';
 
 export default function Search({placeholder}: {placeholder: string}) {
   const searchParams = useSearchParams();
