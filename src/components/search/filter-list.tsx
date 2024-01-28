638e87dfca264343f84a0257ff60f903969dcbbd
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2024-01-28 14:48:05 +0300

    add dropdown filter menu

--- src/components/search/filter-list.tsx
+++ src/components/search/filter-list.tsx
@@ -1,19 +1,31 @@
-import {PathFilterItem, SortFilterItem} from '@/src/lib/definitions';
+import {FilterListItem, PathFilterItem, SortFilterItem} from '@/src/lib/definitions';
 import FilterItem from './filter-item';
+import Dropdown from './dropdown';
+
+function FilterItemList({list}: {list: FilterListItem[]}) {
+  return (
+    <>
+      {list.map((item, i) => (
+        <FilterItem
+          item={item}
+          key={i}
+        />
+      ))}
+    </>
+  );
+}
 
 /* TODO: do the dropdown menu for mobile phone */
-export default function FilterList({list, title}: {list: PathFilterItem[] | SortFilterItem[]; title?: string}) {
+export default function FilterList({list, title}: {list: FilterListItem[]; title?: string}) {
   return (
     <nav>
       <h3 className="text-xs text-neutral-500 dark:text-neutral-400">{title}</h3>
-      <ul>
-        {list.map(item => (
-          <FilterItem
-            item={item}
-            key={item.title}
-          />
-        ))}
+      <ul className="hidden md:block">
+        <FilterItemList list={list} />
       </ul>
+      <div className="block md:hidden">
+        <Dropdown list={list} />
+      </div>
     </nav>
   );
 }
